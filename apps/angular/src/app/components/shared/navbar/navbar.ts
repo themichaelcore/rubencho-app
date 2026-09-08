31f5b9757163d382cc76bea5a6b8937102f95446
Michael Salazar <themichaelcore@gmail.com>
2026-09-07 19:39:12 -0500

    Include logo

--- apps/angular/src/app/components/shared/navbar/navbar.ts
+++ apps/angular/src/app/components/shared/navbar/navbar.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
